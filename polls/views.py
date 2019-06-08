bee69c19c9b04f28ddde24f4ecc85dc0373eff30
Ravi Anand <ravi.anand@nbt.ag>
2019-06-08 19:28:17 +0200

    Add views, templates and layout
    -handle 404 error
    -update gitignore file
    -removed db.sqlite3 file
    -add html pages.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse('This is first view in Django...!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html') long way to render template step 1
+    context = {'latest_question_list': latest_question_list}
+    # return HttpResponse(template.render(context, request)) step 2
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/vote.html', {'question': question})
