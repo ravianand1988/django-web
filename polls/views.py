4f52cba8410818ade5628c267941567ae4f2ee6e
Ravi Anand <ravi.anand@nbt.ag>
2019-06-18 15:47:54 +0200

    Update Custom QuestionAdmin
    -update models, views
    -override base_site.html //but it's not working

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions"""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(choice__isnull=False).distinct().order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
