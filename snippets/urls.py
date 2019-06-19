18e3b1d5ef9828f21579b59aba3b6b2a02979f92
Ravi Anand <ravi.anand@nbt.ag>
2019-06-19 13:23:38 +0200

    Use Class based DRF views.

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
