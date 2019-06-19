acf47e14c137f502f18d87135a0d83c39b063cf7
Ravi Anand <ravi.anand@nbt.ag>
2019-06-19 18:06:29 +0200

    Replace ModelSerializer from HyperlinkedModelSerializer
    -update url with name, it's required with HyperlinkedModelSerializer

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
     path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
