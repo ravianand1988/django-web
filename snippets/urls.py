9fbe298e6775e9d88bc5852effbb99cf2e81354e
Ravi Anand <ravi.anand@nbt.ag>
2019-06-19 17:42:43 +0200

    Add API root
    -listed users and snippet on root level.

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,9 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.SnippetList.as_view()),
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
+    path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('api-auth/', include('rest_framework.urls'))
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,20 @@
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.permisssions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
