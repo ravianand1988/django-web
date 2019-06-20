4aedb0e7d8a375e97cfc3ea76c7adcef38bed001
Ravi Anand <ravi.anand@nbt.ag>
2019-06-20 11:26:50 +0200

    Add DRF default schema

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from snippets.views import SnippetViewSet, UserViewSet, api_root
+
+schema_view = get_schema_view(title='DRF Snippet API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,6 +12,7 @@ router.register(r'snippets', SnippetViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ]
