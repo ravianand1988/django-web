1d921c201b051b8bdb55865e8214949a2a8ee912
Ravi Anand <ravi.anand@nbt.ag>
2019-06-20 10:44:02 +0200

    Use DRF DefaultRouter for ViewSets URLs

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,36 +1,14 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-})
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', snippet_list, name='user-list'),
-    path('users/<int:pk>/', snippet_detail, name='user-detail'),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
