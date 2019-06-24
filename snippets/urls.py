7463a5c4adb7b75f15568b9ad556fe81ef4c38a5
Ravi Anand <ravi.anand@nbt.ag>
2019-06-24 12:05:22 +0200

    Add Publication and Article models to explore M2M in Django.
    -add ViewSets for them.
    -add URL for same.
    -add Django migration for same.
    -add Serializer for same.

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 
-from snippets.views import SnippetViewSet, UserViewSet, api_root
+from snippets.views import SnippetViewSet, UserViewSet, ArticleViewSet, PublicationViewSet
 
 schema_view = get_schema_view(title='DRF Snippet API')
 
@@ -10,7 +10,8 @@ schema_view = get_schema_view(title='DRF Snippet API')
 router = DefaultRouter()
 router.register(r'snippets', SnippetViewSet)
 router.register(r'users', UserViewSet)
-
+router.register(r'articles', ArticleViewSet)
+router.register(r'publications', PublicationViewSet)
 urlpatterns = [
     path('schema/', schema_view),
     path('', include(router.urls)),
